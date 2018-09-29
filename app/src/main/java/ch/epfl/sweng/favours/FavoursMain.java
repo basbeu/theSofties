6c75aacba841fa419b7747c7a0735cabf410ff14
Nicolas Peslerbe <nicolas@Nicolass-MacBook-Pro.local>
2018-09-29 16:09:04 +0200

    Authentification ok

--- app/src/main/java/ch/epfl/sweng/favours/FavoursMain.java
+++ app/src/main/java/ch/epfl/sweng/favours/FavoursMain.java
@@ -5,9 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String TAG = "TheSoftiesApp";
+
+    private RuntimeEnvironment runtimeEnvironment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        runtimeEnvironment = new RuntimeEnvironment();
+
+
+
         setContentView(R.layout.activity_main);
     }
 }
