1a2af50977773bcf3b4e8e09eebe1f06ac021854
Andrea Scalisi <andrea.scalisi@epfl.ch>
2018-10-06 13:02:30 +0200

    Add sample main screen once logged in + menu folder Once the user has logged in, a new screen displays

---
+++ app/src/main/java/ch/epfl/sweng/favours/Logged_in_Screen.java
@@ -0,0 +1,14 @@
+package ch.epfl.sweng.favours;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Logged_in_Screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged_in__screen);
+    }
+
+}
