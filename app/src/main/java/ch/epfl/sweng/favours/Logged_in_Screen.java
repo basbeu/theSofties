87a814fe085a0ed7d917a645e83143e790a3863c
Andrea Scalisi <andrea.scalisi@epfl.ch>
2018-10-07 10:39:39 +0200

    Menu toolbar part 2 Added button to open the menu toolbar Other buttons in the menu still don't have any effect

--- app/src/main/java/ch/epfl/sweng/favours/Logged_in_Screen.java
+++ app/src/main/java/ch/epfl/sweng/favours/Logged_in_Screen.java
@@ -2,13 +2,38 @@ package ch.epfl.sweng.favours;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+
 
 public class Logged_in_Screen extends Activity {
 
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in__screen);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+
+    }
 }
