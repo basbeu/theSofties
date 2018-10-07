33422d3a8f05404320ac68a368845a1f96647d5a
basbeu <bastien.beuchat@epfl.ch>
2018-10-07 15:32:13 +0200

    Add simple User class

---
+++ app/src/main/java/ch/epfl/sweng/favours/User.java
@@ -0,0 +1,33 @@
+package ch.epfl.sweng.favours;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Character sex;
+    private String basedLocation;
+
+    public User(){}
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Character getSex() {
+        return sex;
+    }
+
+    public String getBasedLocation() {
+        return basedLocation;
+    }
+
+}
