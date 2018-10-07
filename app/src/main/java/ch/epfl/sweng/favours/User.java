39217d1ba2ce918a1db9d6b1c424cb4cef41e9a6
Jeremy Mion <jeremy.mion@yahoo.ca>
2018-10-08 00:42:25 +0200

    Declare final constant static field for database access

--- app/src/main/java/ch/epfl/sweng/favours/User.java
+++ app/src/main/java/ch/epfl/sweng/favours/User.java
@@ -1,6 +1,13 @@
 package ch.epfl.sweng.favours;
 
 public class User {
+
+    public static final String FIRST_NAME = "firstName";
+    public static final String LAST_NAME = "lastName";
+    public static final String EMAIL = "email";
+    public static final String SEX = "sex";
+    public static final String BASED_LOCATION = "basedLocation";
+
     private String firstName;
     private String lastName;
     private String email;
