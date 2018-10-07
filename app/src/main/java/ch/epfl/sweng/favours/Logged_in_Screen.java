64db59ad8d9596269a1b955f14cb19bbc0c1474f
Andrea Scalisi <andrea.scalisi@epfl.ch>
2018-10-07 11:58:50 +0200

    Menu toolbar part 3 Can access to the following fragments : profile, favours, settings Remaining : Log out button doesn't have effect yet

--- app/src/main/java/ch/epfl/sweng/favours/Logged_in_Screen.java
+++ app/src/main/java/ch/epfl/sweng/favours/Logged_in_Screen.java
@@ -2,13 +2,18 @@ package ch.epfl.sweng.favours;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 
-public class Logged_in_Screen extends Activity {
+public class Logged_in_Screen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
     @Override
@@ -19,12 +24,22 @@ public class Logged_in_Screen extends Activity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         drawer = findViewById(R.id.drawer_layout);
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+            navigationView.setCheckedItem(R.id.profile);
+        }
+
 
     }
+
+
 
     @Override
     public void onBackPressed() {
@@ -35,5 +50,24 @@ public class Logged_in_Screen extends Activity {
             super.onBackPressed();
         }
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId())
+        {
+            case R.id.profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
+                break;
+            case R.id.favours:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FavoursFragment()).commit();
+                break;
+            case R.id.settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
