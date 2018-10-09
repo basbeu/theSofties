e281a51cbe64035d6955d6ad71e4baca5af00603
basbeu <bastien.beuchat@epfl.ch>
2018-10-09 17:28:14 +0200

    Test git config+

--- app/src/main/java/ch/epfl/sweng/favours/database/Favor.java
+++ app/src/main/java/ch/epfl/sweng/favours/database/Favor.java
@@ -1,6 +1,7 @@
 package ch.epfl.sweng.favours.database;
 
 public class Favor extends DatabaseHandler {
+
     public enum StringFields implements DatabaseStringField {title, ownerID, description}
     public enum IntegerFields implements DatabaseIntField {creationTimestamp}
 
