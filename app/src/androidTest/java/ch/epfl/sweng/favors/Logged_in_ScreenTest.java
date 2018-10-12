e09cfcf244c90b9fa57eb329590750d245fe5f5a
Andrea Scalisi <andrea.scalisi@epfl.ch>
2018-10-12 11:55:43 +0200

    Add all test classes

---
+++ app/src/androidTest/java/ch/epfl/sweng/favors/Logged_in_ScreenTest.java
@@ -0,0 +1,10 @@
+package ch.epfl.sweng.favors;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class Logged_in_ScreenTest {
+
+}
