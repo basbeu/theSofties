ebc131bde243ce193ebc58467b9d73882eac5605
Andrea Scalisi <andrea.scalisi@epfl.ch>
2018-10-13 14:41:11 +0200

    Add configuration to test fragments independently

--- app/src/androidTest/java/ch/epfl/sweng/favors/Logged_in_ScreenTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/favors/Logged_in_ScreenTest.java
@@ -2,9 +2,15 @@ package ch.epfl.sweng.favors;
 
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class Logged_in_ScreenTest {
+    
 
 }
