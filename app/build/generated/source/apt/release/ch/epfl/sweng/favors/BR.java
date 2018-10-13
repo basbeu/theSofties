ab634cc484ba716c7a45376f507edc5b051b2ed4
basbeu <bastien.beuchat@epfl.ch>
2018-10-13 09:51:36 +0200

    Remove build folder

--- app/build/generated/source/apt/release/ch/epfl/sweng/favors/BR.java
+++
@@ -1,6 +0,0 @@
-package ch.epfl.sweng.favors;
-
-public class BR {
-        public static final int _all = 0;
-        public static final int elements = 1;
-}
--- app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="42d7744e-777c-4cc6-8fe9-ca1f4ce53d62")
-public class DataBindingInfo {}
