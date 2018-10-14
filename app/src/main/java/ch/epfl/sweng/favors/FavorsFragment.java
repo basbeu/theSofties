c614f4b714f657c9aa54ca293c74cc294dc683e1
Nicolas Peslerbe <development@peslerbe.com>
2018-10-14 23:27:26 +0200

    Improve the edit favor functionality

--- app/src/main/java/ch/epfl/sweng/favors/FavorsFragment.java
+++ app/src/main/java/ch/epfl/sweng/favors/FavorsFragment.java
@@ -26,18 +26,52 @@ import ch.epfl.sweng.favors.database.FavorRequest;
 import ch.epfl.sweng.favors.databinding.FavorsLayoutBinding;
 
 public class FavorsFragment extends Fragment {
+
     private static final String TAG = "FAVOR_FRAGMENT";
+
     FavorsLayoutBinding binding;
     ObservableArrayList<Favor> favorList;
-    Boolean editing = false;
+
+    public ObservableField<String> favorTitle;
+    public ObservableField<String> favorDescription;
+
+    public ObservableField<String> validationButtonText = new ObservableField<>("--");
+    public ObservableField<String> fragmentTitle = new ObservableField<>("--");
+    public ObservableField<String> validationText = new ObservableField<>("--");
+
+    public final String KEY_FRAGMENT_ID = "fragment_id";
+
+    /**
+     * Load a fragment with a view to edit a favor of the database or to add a new one
+     * While creating the favor fragment, please indicate the favor ID of the favor you want to edit
+     * with the KEY_FRAGMENT_ID key and it'll be loaded directly form the server and updated when
+     * the validation button is clicked
+     *
+     * @param inflater Managed by the system
+     * @param container Managed by the system
+     * @param savedInstanceState Managed by the system
+     * @return The view
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        final Favor newFavor = new Favor();
-
         binding = DataBindingUtil.inflate(inflater, R.layout.favors_layout,container,false);
         binding.setElements(this);
+
+        Favor newFavor;
+        String strtext;
+        if(getArguments() != null && (strtext = getArguments().getString(KEY_FRAGMENT_ID)) != null) {
+            newFavor = new Favor(strtext);
+            updateUI(true);
+        }
+        else{
+            newFavor = new Favor();
+            updateUI(false);
+        }
+
+        favorTitle = newFavor.getObservableObject(Favor.StringFields.title);
+        favorDescription = newFavor.getObservableObject(Favor.StringFields.description);
 
         binding.titleFavor.addTextChangedListener(new TextWatcherCustom() {
             @Override
@@ -89,14 +123,25 @@ public class FavorsFragment extends Fragment {
                     newFavor.set(Favor.StringFields.ownerID, FirebaseAuth.getInstance().getUid());
                     newFavor.updateOnDb();
 
-                    binding.addFavor.setText("Edit your favor");
-                    if(editing) launchToast("Favor edited successfully");
-                    else launchToast("Favor created successfully");
+                    launchToast(validationText.get());
 
-                    editing= true;
+                    updateUI(true);
                 }
         });
         return binding.getRoot();
+    }
+
+    private void updateUI(boolean isEditing){
+        if(isEditing){
+            validationButtonText.set("Edit the favor");
+            fragmentTitle.set("Edit an existing favor");
+            validationText.set("Favor edited successfully");
+        }
+        else{
+            validationButtonText.set("Create the favor");
+            fragmentTitle.set("Create a new favor");
+            validationText.set("Favor created successfully");
+        }
     }
 
     private void launchToast(String text) {
