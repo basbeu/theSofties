ccdc11632ab5da2406c283799ed3b9bc1527efca
Andrea Scalisi <andrea.scalisi@epfl.ch>
2018-10-14 16:56:41 +0200

    Still issues with Mockito
    Error thrown when EditProfileFragmentTest is run
    Powermockito doesn't work yet

--- app/src/main/java/ch/epfl/sweng/favors/database/User.java
+++ app/src/main/java/ch/epfl/sweng/favors/database/User.java
@@ -4,10 +4,15 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class User extends DatabaseHandler {
     private static final String COLLECTION = "users";
+    private FirebaseAuth instance;
 
     private static User user = new User();
     public static User getMain(){
         return user;
+    }
+
+    public FirebaseAuth getInstance() {
+        return instance;
     }
 
     public static void setMain(String id){
@@ -18,13 +23,26 @@ public class User extends DatabaseHandler {
 
     public User(){
         super(StringFields.values(), COLLECTION,FirebaseAuth.getInstance().getUid());
-        if(FirebaseAuth.getInstance().getUid() != null)
+        instance = FirebaseAuth.getInstance();
+        if(instance.getUid() != null){
             updateFromDb();
+        }
+
     }
 
     public User(String id){
         super(StringFields.values(), COLLECTION,id);
-        if(FirebaseAuth.getInstance().getUid() != null)
+        instance = FirebaseAuth.getInstance();
+        if(instance.getUid() != null) {
             updateFromDb();
+        }
+    }
+
+    public User(FirebaseAuth instance){
+        super(StringFields.values(), COLLECTION,instance.getUid());
+        this.instance = instance;
+        if(instance.getUid() != null) {
+            updateFromDb();
+        }
     }
 }
