d3b95fbd657c5fb77d98417c380bcdbd42a76f30
Andrea Scalisi <andrea.scalisi@epfl.ch>
2018-10-16 20:18:24 +0200

    Mock FirebaseAuth
    This fixed EditProfileFragmentTest

--- app/src/androidTest/java/ch/epfl/sweng/favors/EditProfileFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/favors/EditProfileFragmentTest.java
@@ -3,6 +3,9 @@ package ch.epfl.sweng.favors;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -14,6 +17,11 @@ import org.mockito.junit.MockitoRule;
 
 import ch.epfl.sweng.favors.database.User;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.TestCase.assertEquals;
 import static org.mockito.Mockito.when;
 
 
@@ -25,18 +33,25 @@ public class EditProfileFragmentTest {
     @Rule public FragmentTestRule<EditProfileFragment> mFragmentTestRule = new FragmentTestRule<>(EditProfileFragment.class);
     @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
 
-    @Mock User fakeUser;
+   // @Mock User fakeUser;
+    @Mock FirebaseUser fbFakeUser;
+    @Mock FirebaseFirestore fstore;
+    @Mock FirebaseAuth fakeAuth;
 
     @Before
     public void Before(){
+
+        User fakeUser = new User(fakeAuth);
         User.setMain(fakeUser);
-        //when(fakeUser.getInstance().getCurrentUser().getEmail()).thenReturn("email@test.toto");
+        when(fakeAuth.getCurrentUser()).thenReturn(fbFakeUser);
+        when(fbFakeUser.getEmail()).thenReturn("thisisatestemail@email.com");
     }
     @Test
     public void fragment_can_be_instantiated() {
 
-        //mFragmentTestRule.launchActivity(null);
-        //onView(withId(R.id.profileTitle)).check(matches(isDisplayed()));
+
+        mFragmentTestRule.launchActivity(null);
+        onView(withId(R.id.profileTitle)).check(matches(isDisplayed()));
     }
     
 }
