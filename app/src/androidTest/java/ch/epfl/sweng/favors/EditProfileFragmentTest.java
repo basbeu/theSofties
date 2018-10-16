bc7788c79b45834df78778126240b65e0addc664
Andrea Scalisi <andrea.scalisi@epfl.ch>
2018-10-16 17:59:30 +0200

    Get rid of Powermock

--- app/src/androidTest/java/ch/epfl/sweng/favors/EditProfileFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/favors/EditProfileFragmentTest.java
@@ -5,35 +5,20 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 
 @RunWith(AndroidJUnit4.class)
-/*@RunWith(PowerMockRunner.class)
-@PrepareForTest(com.google.firebase.auth.FirebaseAuth.class)
-@PowerMockRunnerDelegate(JUnit4.class)*/
 
 
 public class EditProfileFragmentTest {
 
     @Rule
     public FragmentTestRule<EditProfileFragment> mFragmentTestRule = new FragmentTestRule<>(EditProfileFragment.class);
-    //@Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+    @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
 
-    /*@Mock FirebaseAuth mockedAuth;
-    public User testUser;
-
-
-    @Before
-    public void before(){
-        PowerMockito.mockStatic(FirebaseAuth.class);
-        when(mockedAuth.getUid()).thenReturn("7777");
-        when(mockedAuth.getCurrentUser().getEmail()).thenReturn("toto@email.com");
-        testUser = new User(mockedAuth);
-        when(User.getMain()).thenReturn(testUser);
-    }
-
-    */
-
+ 
     @Test
     public void fragment_can_be_instantiated() {
 
