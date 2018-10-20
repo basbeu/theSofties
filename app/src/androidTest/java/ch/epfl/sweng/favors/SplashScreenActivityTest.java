49b5e518065e7c3ade4bf0f6d9b404e40aed5591
Andrea Scalisi <andrea.scalisi@epfl.ch>
2018-10-20 18:25:32 +0200

    Add test for SplashScreenActivity

--- app/src/androidTest/java/ch/epfl/sweng/favors/SplashScreenActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/favors/SplashScreenActivityTest.java
@@ -11,17 +11,32 @@ import ch.epfl.sweng.favors.SplashScreenActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.fail;
 
 @RunWith(AndroidJUnit4.class)
 
 public class SplashScreenActivityTest {
-    @Rule
-    public final ActivityTestRule<SplashScreenActivity> mActivityRule =
-            new ActivityTestRule<>(SplashScreenActivity.class);
+    @Rule public final ActivityTestRule<SplashScreenActivity> mActivityRule = new ActivityTestRule<>(SplashScreenActivity.class);
+
     @Test
     public void testSplashScreen() {
-        onView(withId(R.id.logoFavors)).check(matches(withId(R.id.logoFavors)));
+        onView(withId(R.id.logoFavors)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void splashScreenDoesNotLastForever(){
+        try{
+            Thread.sleep(4000);
+            onView(withId(R.id.welcomeMessageText)).check(matches(isDisplayed()));
+
+        }catch(Exception e){
+            fail("Can't sleep");
+        }
     }
 }
