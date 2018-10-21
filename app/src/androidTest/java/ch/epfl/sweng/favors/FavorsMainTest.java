76ebad19b577a9221965eaadaca69b5acb7d4b55
Andrea Scalisi <andrea.scalisi@epfl.ch>
2018-10-21 13:36:50 +0200

    Add a line in Travis.yml to give GPS authorization

--- app/src/androidTest/java/ch/epfl/sweng/favors/FavorsMainTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/favors/FavorsMainTest.java
@@ -2,7 +2,10 @@ package ch.epfl.sweng.favors;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.app.ActivityCompat;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,20 +23,31 @@ import static junit.framework.TestCase.assertEquals;
 
 
 public class FavorsMainTest {
-    //@Rule public ActivityTestRule<FavorsMain> activityActivityTestRule = new ActivityTestRule<FavorsMain>(FavorsMain.class);
+    @Rule public ActivityTestRule<FavorsMain> activityActivityTestRule = new ActivityTestRule<>(FavorsMain.class);
+
+
+    @Before
+    public void Before(){
+        ActivityCompat.setPermissionCompatDelegate(new LocationDelegate());
+    }
 
     @Test
     public void registerView(){
-       /* onView(withId(R.id.registerButton)).perform(click());
+        onView(withId(R.id.registerButton)).perform(click());
         onView(withId(R.id.authentificationButton)).check(matches(isDisplayed()));
-        onView(withId(R.id.loginMessageText)).check(matches(withText("Welcome here! Just some small steps...")));*/
+        onView(withId(R.id.loginMessageText)).check(matches(withText("Welcome here! Just some small steps...")));
     }
 
     @Test
     public void loginView(){
-        /*onView(withId(R.id.loginButton)).perform(click());
+        onView(withId(R.id.loginButton)).perform(click());
         onView(withId(R.id.authentificationButton)).check(matches(isDisplayed()));
-        onView(withId(R.id.loginMessageText)).check(matches(withText("Please enter your login informations:")));*/
+        onView(withId(R.id.loginMessageText)).check(matches(withText("Please enter your login informations:")));
+    }
+
+    @After
+    public void After(){
+        ActivityCompat.setPermissionCompatDelegate(null);
     }
     
 }
