46bfbc8905d666b3f1f33df58e175ac82dcac56a
basbeu <bastien.beuchat@epfl.ch>
2018-10-23 18:18:36 +0200

    Tests works in local

--- app/src/androidTest/java/ch/epfl/sweng/favors/FavorsMainTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/favors/FavorsMainTest.java
@@ -3,10 +3,6 @@ package ch.epfl.sweng.favors;
 import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.test.uiautomator.UiDevice;
-import android.support.test.uiautomator.UiObject;
-import android.support.test.uiautomator.UiObjectNotFoundException;
-import android.support.test.uiautomator.UiSelector;
 import android.support.v4.app.ActivityCompat;
 
 import org.junit.After;
@@ -32,11 +28,11 @@ import static junit.framework.TestCase.assertEquals;
 
 public class FavorsMainTest {
     @Rule public ActivityTestRule<FavorsMain> activityActivityTestRule = new ActivityTestRule<>(FavorsMain.class, true, false);
-    private UiDevice device;
+    //private UiDevice device;
 
     @Before
     public void Before(){
-        device = UiDevice.getInstance(getInstrumentation());
+        //device = UiDevice.getInstance(getInstrumentation());
         ActivityCompat.setPermissionCompatDelegate(new LocationDelegate());
     }
 
@@ -48,15 +44,10 @@ public class FavorsMainTest {
 
         activityActivityTestRule.launchActivity(null);
 
-        UiObject allowButton = device.findObject(new UiSelector().text("ALLOW"));
-        UiObject denyButton = device.findObject(new UiSelector().text("DENY"));
+        //UiObject allowButton = device.findObject(new UiSelector().text("ALLOW"));
+        //UiObject denyButton = device.findObject(new UiSelector().text("DENY"));
 
-        if(denyButton.exists()){
-           denyButton.click();
-        }
-        else if(allowButton.exists()){
-            allowButton.click();
-        }
+        /**/
 
 
         onView(withId(R.id.registerButton)).perform(click());
@@ -73,7 +64,7 @@ public class FavorsMainTest {
 
         //denyCurrentPermission(UiDevice.getInstance(getInstrumentation()));
 
-        UiObject allowButton = device.findObject(new UiSelector().text("ALLOW"));
+        /*UiObject allowButton = device.findObject(new UiSelector().text("ALLOW"));
         UiObject denyButton = device.findObject(new UiSelector().text("DENY"));
 
         if(denyButton.exists()){
@@ -81,7 +72,7 @@ public class FavorsMainTest {
         }
         else if(allowButton.exists()){
             allowButton.click();
-        }
+        }*/
 
         /*UiObject loginButton = device.findObject(new UiSelector().text("LOGIN"));
         if(loginButton.exists()){
@@ -94,7 +85,7 @@ public class FavorsMainTest {
     }
 
 
-
+/*
     public static void assertViewWithTextIsVisible(UiDevice device, String text) {
         UiObject allowButton = device.findObject(new UiSelector().text(text));
         if (!allowButton.exists()) {
@@ -111,7 +102,12 @@ public class FavorsMainTest {
         UiObject allowButton = device.findObject(new UiSelector().text("ALLOW"));
         allowButton.click();
     }
+*/
 
+    @After
+    public void After(){
+        ActivityCompat.setPermissionCompatDelegate(null);
+    }
 
 
 
