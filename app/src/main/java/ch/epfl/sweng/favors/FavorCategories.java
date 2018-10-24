5160ee0b2a47f21fed8236abd9104e1e4b273cab
Jeremy Mion <jeremy.mion@yahoo.ca>
2018-10-24 10:21:40 +0200

    reformat favorCategory

--- app/src/main/java/ch/epfl/sweng/favors/FavorCategories.java
+++ app/src/main/java/ch/epfl/sweng/favors/FavorCategories.java
@@ -1,9 +1,24 @@
-package ch.epfl.sweng.favors.database;
+package ch.epfl.sweng.favors;
 
 public interface FavorCategories {
-     String[] favorCategories = {"tutoring", "writing", "designing", "coding",
-            "event preparation", "bring groceries", "cooking", "delivery", "driver",
-            "place to sleep", "moving", "reparations", "installation", "photography",
-            "entertainer", "magician", "clown", "birthday party"
+     String[] favorCategories = {
+             "tutoring",
+             "writing",
+             "designing",
+             "coding",
+            "event preparation",
+             "bring groceries",
+             "cooking",
+             "delivery",
+             "driver",
+            "place to sleep",
+             "moving",
+             "reparations",
+             "installation",
+             "photography",
+            "entertainer",
+             "magician",
+             "clown",
+             "birthday party"
     };
 }
