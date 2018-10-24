07c0588b64c2d87d85b4bacbae495ef9e27bf504
Jeremy Mion <jeremy.mion@yahoo.ca>
2018-10-24 08:01:07 +0200

    Extra test

--- app/src/androidTest/java/ch/epfl/sweng/favors/FavorsCreateFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/favors/FavorsCreateFragmentTest.java
@@ -2,14 +2,25 @@ package ch.epfl.sweng.favors;
 
 import android.support.test.runner.AndroidJUnit4;
 
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import ch.epfl.sweng.favors.database.DatabaseHandler;
+import ch.epfl.sweng.favors.database.User;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 @RunWith(AndroidJUnit4.class)
 public class FavorsCreateFragmentTest {
@@ -23,5 +34,46 @@ public class FavorsCreateFragmentTest {
         mFragmentTestRule.launchActivity(null);
         onView(withId(R.id.createFavorTitle)).check(matches(isDisplayed()));
     }
+
+    @Test
+    public void isStringValide_fails_on_null(){
+        FavorCreateFragment fc = new FavorCreateFragment();
+        try {
+            Method stringvalide = FavorCreateFragment.class.getDeclaredMethod("isStringValid", String.class);
+            stringvalide.setAccessible(true);
+            boolean b = (boolean)stringvalide.invoke(fc, "");
+            assertThat(b,is(false));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            assertThat("Error",true,is(false));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            assertThat("Error",true,is(false));
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+            assertThat("Error",true,is(false));
+        }
+    }
+
+    @Test
+    public void isStringValide_succeds_on_HelloWorld(){
+        FavorCreateFragment fc = new FavorCreateFragment();
+        try {
+            Method stringvalide = FavorCreateFragment.class.getDeclaredMethod("isStringValid", String.class);
+            stringvalide.setAccessible(true);
+            boolean b = (boolean)stringvalide.invoke(fc, "HelloWorld");
+            assertThat(b,is(true));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            assertThat("Error",true,is(false));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            assertThat("Error",true,is(false));
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+            assertThat("Error",true,is(false));
+        }
+    }
+
     
 }
