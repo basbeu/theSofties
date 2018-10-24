d00bff371c0dec41851bc4ecb37650788b78a3f9
basbeu <bastien.beuchat@epfl.ch>
2018-10-24 05:31:17 +0200

    Change UIautomator by espresso for click during AuthenticationProcessTests

--- app/src/androidTest/java/ch/epfl/sweng/favors/AuthentificationProcessTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/favors/AuthentificationProcessTest.java
@@ -7,6 +7,7 @@ import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiSelector;
 
+import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,7 +27,10 @@ import ch.epfl.sweng.favors.database.User;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -58,8 +62,11 @@ public class AuthentificationProcessTest {
         when(fakeAuth.getCurrentUser()).thenReturn(fbFakeUser);
         when(fakeAuth.signInWithEmailAndPassword(FAKE_EMAIL,FAKE_PASSWORD)).thenReturn(Tasks.forResult(null));
         when(fakeAuth.signInWithEmailAndPassword(FAKE_WRONG_EMAIL,FAKE_PASSWORD)).thenReturn(Tasks.forCanceled());
+        when(fakeAuth.createUserWithEmailAndPassword(FAKE_EMAIL,FAKE_PASSWORD)).thenReturn(Tasks.forResult(null));
+        when(fakeAuth.createUserWithEmailAndPassword(FAKE_WRONG_EMAIL,FAKE_PASSWORD)).thenReturn(Tasks.forCanceled());
         when(fbFakeUser.getEmail()).thenReturn(FAKEEMAIL);
         when(fbFakeUser.isEmailVerified()).thenReturn(true);
+        when(fbFakeUser.sendEmailVerification()).thenReturn(Tasks.forResult((Void)null));
     }
 
     @Test
@@ -70,11 +77,15 @@ public class AuthentificationProcessTest {
         activityActivityTestRule.launchActivity(intent);
         // Check if the title correspond to a login title
         onView(withId(R.id.loginMessageText)).check(matches(isDisplayed()));
+        //login process
         onView(withId(R.id.emailTextField)).perform(replaceText(FAKE_EMAIL));
-        onView(withId(R.id.passwordTextField)).perform(replaceText(FAKE_PASSWORD));
-        // Add button click and see if the function is call if the password if valid or error display if not
-        UiObject loginButton = device.findObject(new UiSelector().text("LOGIN"));
-        loginButton.click();
+        onView(withId(R.id.passwordTextField)).perform(replaceText(FAKE_PASSWORD)).perform(closeSoftKeyboard());
+        onView(withId(R.id.authentificationButton)).perform(scrollTo(), click());
+        /*UiObject loginButton = device.findObject(new UiSelector().text("LOGIN"));
+        loginButton.click();*/
+
+        //Check that we are login in the app
+        onView(withId(R.id.profileTitle)).check(matches(isDisplayed()));
     }
 
     @Test
@@ -85,26 +96,54 @@ public class AuthentificationProcessTest {
         activityActivityTestRule.launchActivity(intent);
         // Check if the title correspond to a login title
         onView(withId(R.id.loginMessageText)).check(matches(isDisplayed()));
+        //wrong login process
         onView(withId(R.id.emailTextField)).perform(replaceText(FAKE_WRONG_EMAIL));
-        onView(withId(R.id.passwordTextField)).perform(replaceText(FAKE_PASSWORD));
-        // Add button click and see if the function is call if the password if valid or error display if not
-        UiObject loginButton = device.findObject(new UiSelector().text("LOGIN"));
-        loginButton.click();
+        onView(withId(R.id.passwordTextField)).perform(replaceText(FAKE_PASSWORD)).perform(closeSoftKeyboard());
+        onView(withId(R.id.authentificationButton)).perform(scrollTo(), click());
+
+        /*UiObject loginButton = device.findObject(new UiSelector().text("LOGIN"));
+        loginButton.click();*/
+        //Check that the login is failed
         assertEquals("Wrong email or password or email not verified\nPlease try again",activityActivityTestRule.getActivity().requirementsText.get());
     }
 
     @Test
-    public void register(){
+    public void registerSucceed() throws Exception{
         ActivityTestRule<AuthentificationProcess> activityActivityTestRule = new ActivityTestRule<>(AuthentificationProcess.class);
         Intent intent = new Intent();
         intent.putExtra(AuthentificationProcess.AUTHENTIFICATION_ACTION, AuthentificationProcess.Action.Register);
         activityActivityTestRule.launchActivity(intent);
         // Check if the title correspond to a register title
         onView(withId(R.id.loginMessageText)).check(matches(isDisplayed()));
-        onView(withId(R.id.emailTextField)).perform(replaceText("toto@email.com"));
-        onView(withId(R.id.passwordTextField)).perform(replaceText("abcd1234"));
+        //register process
+        onView(withId(R.id.emailTextField)).perform(replaceText(FAKE_EMAIL));
+        onView(withId(R.id.passwordTextField)).perform(replaceText(FAKE_PASSWORD)).perform(closeSoftKeyboard());
+        onView(withId(R.id.authentificationButton)).perform(scrollTo(), click());
+        /*UiObject registerButton = device.findObject(new UiSelector().text("REGISTER"));
+        registerButton.click();*/
+        //Check that we load setUserInfo
+        onView(withId(R.id.submit)).check(matches(isDisplayed()));
+
     }
 
+    @Test
+    public void registerFail() throws Exception{
+        ActivityTestRule<AuthentificationProcess> activityActivityTestRule = new ActivityTestRule<>(AuthentificationProcess.class);
+        Intent intent = new Intent();
+        intent.putExtra(AuthentificationProcess.AUTHENTIFICATION_ACTION, AuthentificationProcess.Action.Register);
+        activityActivityTestRule.launchActivity(intent);
+        // Check if the title correspond to a register title
+        onView(withId(R.id.loginMessageText)).check(matches(isDisplayed()));
+        //register process
+        onView(withId(R.id.emailTextField)).perform(replaceText(FAKE_WRONG_EMAIL));
+        onView(withId(R.id.passwordTextField)).perform(replaceText(FAKE_PASSWORD)).perform(closeSoftKeyboard());
+        onView(withId(R.id.authentificationButton)).perform(scrollTo(), click());
+        /*UiObject registerButton = device.findObject(new UiSelector().text("REGISTER"));
+        registerButton.click();*/
+        //Check that the register process failed
+        assertEquals("Register failed, please try again",activityActivityTestRule.getActivity().requirementsText.get());
+
+    }
 
 
 }
