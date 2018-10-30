473a66289a9c3ab157323629e23513b418378126
Nicolas Peslerbe <development@peslerbe.com>
2018-10-30 22:51:23 +0100

    Add setup of test mode variables

--- app/src/androidTest/java/ch/epfl/sweng/favors/main/BigTestAuth.java
+++ app/src/androidTest/java/ch/epfl/sweng/favors/main/BigTestAuth.java
@@ -71,7 +71,7 @@ public class BigTestAuth {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(7000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
