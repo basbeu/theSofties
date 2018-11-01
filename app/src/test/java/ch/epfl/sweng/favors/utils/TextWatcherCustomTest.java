f5d9f94b31893571a7bc0f23d32328882217d628
basbeu <bastien.beuchat@epfl.ch>
2018-11-01 15:34:05 +0100

    Add Jeniffer's Test implementation

--- app/src/test/java/ch/epfl/sweng/favors/utils/TextWatcherCustomTest.java
+++ app/src/test/java/ch/epfl/sweng/favors/utils/TextWatcherCustomTest.java
@@ -1,4 +1,15 @@
 package ch.epfl.sweng.favors.utils;
 
+import org.junit.Test;
+
 public class TextWatcherCustomTest {
+    class TWC extends TextWatcherCustom {}
+
+    @Test
+    public void allMethodsExist(){
+        TWC twc = new TWC();
+        twc.beforeTextChanged(null, 0, 0, 0);
+        twc.onTextChanged(null, 0, 0, 0);
+        twc.afterTextChanged(null);
+    }
 }
