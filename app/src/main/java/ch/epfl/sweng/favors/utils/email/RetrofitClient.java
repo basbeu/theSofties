28dfbc4523c9b833e3e2386e2250ee31b1232ff5
basbeu <bastien.beuchat@epfl.ch>
2018-11-12 10:54:53 +0100

    Remove API key

--- app/src/main/java/ch/epfl/sweng/favors/utils/email/RetrofitClient.java
+++ app/src/main/java/ch/epfl/sweng/favors/utils/email/RetrofitClient.java
@@ -29,7 +29,7 @@ import static java.net.HttpURLConnection.HTTP_OK;
 
     private static final String API_USERNAME = "api";
 
-    private static final String API_PASSWORD = "3b8cc2b3c0107c42a11cb08cecfe5c6d-4412457b-89b86f77";
+    private static final String API_PASSWORD = "";
 
     private static final String AUTH = "Basic " + Base64.encodeToString((API_USERNAME+":"+API_PASSWORD).getBytes(), Base64.NO_WRAP);
 
