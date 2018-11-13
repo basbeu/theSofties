9dbee2867e4c7012f877cf8fc1735f8b4bb12d40
Nicolas Peslerbe <development@peslerbe.com>
2018-11-14 00:28:05 +0100

    Chenging User request action and adding test interface for fagment intent

--- app/src/main/java/ch/epfl/sweng/favors/favors/FavorPosterDetailView.java
+++ app/src/main/java/ch/epfl/sweng/favors/favors/FavorPosterDetailView.java
@@ -1,65 +1,52 @@
 package ch.epfl.sweng.favors.favors;
 
-import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
 import android.databinding.DataBindingUtil;
-import android.databinding.Observable;
 import android.databinding.ObservableField;
-import android.databinding.ObservableList;
-import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import ch.epfl.sweng.favors.R;
-import ch.epfl.sweng.favors.database.Favor;
 import ch.epfl.sweng.favors.database.UserRequest;
-import ch.epfl.sweng.favors.databinding.FragmentFavorDetailViewBinding;
-
-import ch.epfl.sweng.favors.database.Database;
 import ch.epfl.sweng.favors.database.User;
 import ch.epfl.sweng.favors.databinding.FragmentFavorPosterDetailViewBinding;
-import ch.epfl.sweng.favors.utils.ExecutionMode;
 
 public class FavorPosterDetailView extends android.support.v4.app.Fragment {
-    FragmentFavorDetailViewBinding binding;
 
-    public ObservableField<String> ownerID;
-    private User user;
+    private static String TAG = "FAVORS_POSTER_DETAIL_VIEW";
+    public static final String OWNER_EMAIL = "ownerEMAIL";
+
+
     public ObservableField<String> firstName = new ObservableField<>();
     public ObservableField<String> lastName = new ObservableField<>();
     public ObservableField<String> sex = new ObservableField<>();
-    public FavorPosterDetailView() {
-        // Required empty public constructor
-    }
 
     private String ownerEmail;
-    public static final String OWNER_EMAIL = "ownerEMAIL";
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
-        if(!ExecutionMode.getInstance().isTest()){
-
+        if(getArguments() != null && getArguments().getString(OWNER_EMAIL) != null){
             ownerEmail = getArguments().getString(OWNER_EMAIL);
-            Log.w("TAG",ownerEmail);
-
-            User favorCreatorUser = UserRequest.getWithEmail(ownerEmail);
-            firstName = favorCreatorUser.getObservableObject(User.StringFields.firstName);
-            lastName = favorCreatorUser.getObservableObject(User.StringFields.lastName);
-            sex = favorCreatorUser.getObservableObject(User.StringFields.sex);
         }
-
+        else if(getActivity() != null && getActivity().getIntent() != null &&
+                getActivity().getIntent().getStringExtra(OWNER_EMAIL) != null){
+            ownerEmail = getActivity().getIntent().getStringExtra(OWNER_EMAIL);
+        }
         else{
-            firstName.set("Toto");
-            lastName.set("Tata");
-            sex.set("M");
+            Log.e(TAG, "Trying to intent a the fragment without the email of the favor owner");
         }
+
+        User favorCreatorUser = UserRequest.getWithEmail(ownerEmail);
+        firstName = favorCreatorUser.getObservableObject(User.StringFields.firstName);
+        lastName = favorCreatorUser.getObservableObject(User.StringFields.lastName);
+        sex = favorCreatorUser.getObservableObject(User.StringFields.sex);
+
 
     }
 
