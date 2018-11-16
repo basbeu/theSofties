11c69a2c7331737845ad3d78edb56a2d682a645c
Nicolas Peslerbe <development@peslerbe.com>
2018-11-16 06:06:47 +0100

    Integration map module

--- app/src/main/java/ch/epfl/sweng/favors/favors/FavorsMap.java
+++ app/src/main/java/ch/epfl/sweng/favors/favors/FavorsMap.java
@@ -7,13 +7,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import ch.epfl.sweng.favors.R;
 import ch.epfl.sweng.favors.databinding.FavorsMapBinding;
 
 /**
  * Fragment that displays the list of favor and allows User to sort it and to search in it
  */
-public class FavorsMap extends android.support.v4.app.Fragment {
+public class FavorsMap extends android.support.v4.app.Fragment implements OnMapReadyCallback {
     private static final String TAG = "FAVORS_MAP";
 
     FavorsMapBinding binding;
@@ -26,6 +33,33 @@ public class FavorsMap extends android.support.v4.app.Fragment {
 
 
         return binding.getRoot();
+    }
+
+    private GoogleMap mMap;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
 
