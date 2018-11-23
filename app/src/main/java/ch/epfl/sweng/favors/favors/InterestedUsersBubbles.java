67ee423cadf0e343ad02bba4579d275f1f6233ab
alexander-san <alexander.sanchez@epfl.ch>
2018-11-23 13:08:01 +0100

    Add onCreate to Bubble class

--- app/src/main/java/ch/epfl/sweng/favors/favors/InterestedUsersBubbles.java
+++ app/src/main/java/ch/epfl/sweng/favors/favors/InterestedUsersBubbles.java
@@ -1,20 +1,46 @@
 package ch.epfl.sweng.favors.favors;
 
 import android.content.res.TypedArray;
+import android.databinding.DataBindingUtil;
+import android.databinding.Observable;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.igalata.bubblepicker.BubblePickerListener;
 import com.igalata.bubblepicker.adapter.BubblePickerAdapter;
 import com.igalata.bubblepicker.model.BubbleGradient;
 import com.igalata.bubblepicker.model.PickerItem;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import ch.epfl.sweng.favors.R;
+import ch.epfl.sweng.favors.authentication.Authentication;
+import ch.epfl.sweng.favors.database.Favor;
+import ch.epfl.sweng.favors.database.FavorRequest;
+import ch.epfl.sweng.favors.database.ObservableArrayList;
+import ch.epfl.sweng.favors.databinding.FavorsBinding;
 
 public class InterestedUsersBubbles extends android.support.v4.app.Fragment {
 
-    final String[] titles = getResources().getStringArray(R.array.countries);
-    final TypedArray colors = getResources().obtainTypedArray(R.array.colors);
-    final TypedArray images = getResources().obtainTypedArray(R.array.images);
+    FavorsBinding binding;
+
+    final String[] titles = null;//getResources().getStringArray(R.array.countries);
+    final TypedArray colors = null;//getResources().obtainTypedArray(R.array.colors);
+    final TypedArray images = null; //getResources().obtainTypedArray(R.array.images);
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.bubbles,container,false);
+        binding.setElements(this);
+
+        return binding.getRoot();
+    }
 
     picker.setAdapter(new BubblePickerAdapter() {
         @Override
