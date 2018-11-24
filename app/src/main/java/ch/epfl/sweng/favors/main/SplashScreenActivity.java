dee4be3dab578ad06f3b5de9941e008925c888e1
Jeremy Mion <jeremy.mion@yahoo.ca>
2018-11-25 00:30:38 +0100

    Pull user data form db on splash screen
    This allows for the user informaiton to be loaded when the app starts. It makes use of the splash screen that we have made.

--- app/src/main/java/ch/epfl/sweng/favors/main/SplashScreenActivity.java
+++ app/src/main/java/ch/epfl/sweng/favors/main/SplashScreenActivity.java
@@ -9,7 +9,9 @@ import android.widget.TextView;
 
 import ch.epfl.sweng.favors.BuildConfig;
 import ch.epfl.sweng.favors.R;
+import ch.epfl.sweng.favors.authentication.Authentication;
 import ch.epfl.sweng.favors.database.Database;
+import ch.epfl.sweng.favors.database.User;
 import ch.epfl.sweng.favors.utils.ExecutionMode;
 
 public class SplashScreenActivity extends Activity {
@@ -27,6 +29,7 @@ public class SplashScreenActivity extends Activity {
         // get text view and set the version number in the splash screen
         TextView textView = findViewById(R.id.versionNumber);
         textView.setText(versionName);
+        Database.getInstance().updateFromDb(User.getMain());
         new Handler().postDelayed(() -> {
             Intent intent = new Intent(getApplicationContext(), FavorsMain.class);
             startActivity(intent);
