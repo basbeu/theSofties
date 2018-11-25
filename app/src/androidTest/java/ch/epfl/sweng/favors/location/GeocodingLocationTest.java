4eeab24d8de74d9815af114e68461a5f7594d390
basbeu <bastien.beuchat@epfl.ch>
2018-11-25 23:28:26 +0100

    Improve the test  getAddressFromLocationSuccessMessageTest

--- app/src/androidTest/java/ch/epfl/sweng/favors/location/GeocodingLocationTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/favors/location/GeocodingLocationTest.java
@@ -1,24 +1,28 @@
 package ch.epfl.sweng.favors.location;
 
 
-import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import ch.epfl.sweng.favors.R;
-import ch.epfl.sweng.favors.favors.FavorCreateFragment;
 import ch.epfl.sweng.favors.favors.FavorsMap;
+import ch.epfl.sweng.favors.utils.ExecutionMode;
 import ch.epfl.sweng.favors.utils.FragmentTestRule;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
 
 @RunWith(AndroidJUnit4.class)
 public class GeocodingLocationTest {
@@ -26,9 +30,14 @@ public class GeocodingLocationTest {
     @Rule
     public FragmentTestRule<FavorsMap> mFragmentTestRule = new FragmentTestRule<>(FavorsMap.class);
 
+    @Before
+    public void Before() {
+        ExecutionMode.getInstance().setTest(true);
+    }
 
+    // test that check that getAddressFromLocation() sends the right messages on success
     @Test
-    public void getAddressFromLocationTest(){
+    public void getAddressFromLocationSuccessMessageTest(){
         mFragmentTestRule.launchActivity(null);
         onView(ViewMatchers.withId(R.id.mapView)).check(matches(isDisplayed()));
         if (Looper.myLooper() == null) {
@@ -36,6 +45,23 @@ public class GeocodingLocationTest {
         }
 
         GeocodingLocation locationAddress = new GeocodingLocation();
-        locationAddress.getAddressFromLocation("Lausanne", mFragmentTestRule.getFragment().getContext(), new Handler());
+
+        Handler.Callback callback = new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                if(msg.what == 1){
+                    Bundle bundle = msg.getData();
+                    assertEquals(FakeGeocoder.FAKE_LATITUDE, bundle.getDouble("latitude"));
+                    assertEquals(FakeGeocoder.FAKE_LONGITUDE, bundle.getDouble("longitude"));
+                    Looper.myLooper().quitSafely();
+                }
+                return false;
+            }
+        };
+
+        Handler handler = new Handler(callback);
+        locationAddress.getAddressFromLocation("Lausanne", mFragmentTestRule.getFragment().getContext(), handler);
+
+        Looper.loop();
     }
 }
--- app/src/main/java/ch/epfl/sweng/favors/location/GeocodingLocation.java
+++ app/src/main/java/ch/epfl/sweng/favors/location/GeocodingLocation.java
@@ -12,7 +12,11 @@ import java.util.List;
 
 public class GeocodingLocation {
 
+    public static final String KEY_LATITUDE = "latitude";
+    public static final String KEY_LONGITUDE = "longitude";
+
     private static final String TAG = "GeocodingLocation";
+
 
     public static void getAddressFromLocation(final String locationAddress,
                                               final Context context, final Handler handler) {
@@ -39,8 +43,8 @@ public class GeocodingLocation {
                     if (latitude != null && longitude != null) {
                         message.what = 1;
                         Bundle bundle = new Bundle();
-                        bundle.putDouble("latitude", latitude);
-                        bundle.putDouble("longitude", longitude);
+                        bundle.putDouble(KEY_LATITUDE, latitude);
+                        bundle.putDouble(KEY_LONGITUDE, longitude);
                         message.setData(bundle);
                     } else {
                         message.what = 2;
