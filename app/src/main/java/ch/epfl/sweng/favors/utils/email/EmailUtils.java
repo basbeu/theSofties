2ba80de209ad371329671d57693f44cb5199b769
Jeremy Mion <jeremy.mion@yahoo.ca>
2018-12-03 21:38:58 +0100

    Revert "Refactor Email class"
    This reverts commit aefdf821b071e1252ac1d3590deb364a812baeac.

--- app/src/main/java/ch/epfl/sweng/favors/utils/email/EmailUtils.java
+++ app/src/main/java/ch/epfl/sweng/favors/utils/email/EmailUtils.java
@@ -1,5 +1,9 @@
 package ch.epfl.sweng.favors.utils.email;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
 import ch.epfl.sweng.favors.database.ApiKeys;
 import ch.epfl.sweng.favors.database.Database;
 
@@ -14,9 +18,15 @@ public class EmailUtils {
      * Testing details implementation:
      * If ExecutionMode.getInstance().setInvalidAuthTest(true) then the email will fail to be send and toast for a failed message will be shown
      *
-     * @param email
+     * @param from - the originating source of the email
+     * @param to - whom the email will be send
+     * @param subject - subject/header of the email
+     * @param message - the main body of the email
+     * @param context - the current context that is using this method
+     * @param successMsg - text that will be displayed as a toast if the email is successfully send
+     * @param failureMsg - text that will be displayed as a toast if the email fails to be send
      */
-    public static void sendEmail(Email email){
+    public static void sendEmail(@NonNull String from, @NonNull String to, String subject, String message, @NonNull Context context, @NonNull String successMsg, @NonNull String failureMsg){
 
         //Ensure that API Keys are up to date before calling the API
         ApiKeys key = ApiKeys.getInstance();
@@ -24,8 +34,8 @@ public class EmailUtils {
 
             RetrofitDispatcher.getInstance()
                     .getApi()
-                    .sendEmail(email.getFrom(), email.getTo(), email.getSubject(), email.getMessage())
-                    .enqueue(RetrofitDispatcher.getInstance().getCallback(email.getContext(), email.getSuccessMsg(), email.getFailureMsg()));
+                    .sendEmail(from, to, subject, message)
+                    .enqueue(RetrofitDispatcher.getInstance().getCallback(context, successMsg, failureMsg));
         });
 
     }
