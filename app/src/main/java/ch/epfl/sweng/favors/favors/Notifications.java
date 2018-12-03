becabee72817873683853219a757da1a10a2e92b
Unknown <charline.montial@epfl.ch>
2018-12-03 14:12:13 +0100

    Clean notifications fragment class

--- app/src/main/java/ch/epfl/sweng/favors/favors/Notifications.java
+++ app/src/main/java/ch/epfl/sweng/favors/favors/Notifications.java
@@ -15,49 +15,19 @@ import ch.epfl.sweng.favors.databinding.FavorsListBinding;
 import ch.epfl.sweng.favors.databinding.FragmentNotificationsBinding;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Notifications.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Notifications#newInstance} factory method to
- * create an instance of this fragment.
+ * The fragment which displays the notification of the current user authenticated.
  */
 public class Notifications extends Fragment {
     FragmentNotificationsBinding binding;
     ObservableArrayList<Notification> notificationsList = new ObservableArrayList<>();
-    FavorListAdapter listAdapter;
+    NotificationListAdapter listAdapter;
 
-    private OnFragmentInteractionListener mListener;
 
-    public Notifications() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Notifications.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Notifications newInstance(String param1, String param2) {
-        Notifications fragment = new Notifications();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -67,42 +37,4 @@ public class Notifications extends Fragment {
         return inflater.inflate(R.layout.fragment_notifications, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
