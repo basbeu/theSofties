25fdd5335673e7691a64475dc798c58813cce036
Unknown <charline.montial@epfl.ch>
2018-12-12 00:39:58 +0100

    UI delete account password text watcher added

--- app/src/main/java/ch/epfl/sweng/favors/profile/ProfileFragment.java
+++ app/src/main/java/ch/epfl/sweng/favors/profile/ProfileFragment.java
@@ -2,22 +2,29 @@ package ch.epfl.sweng.favors.profile;
 
 import android.databinding.DataBindingUtil;
 import android.databinding.Observable;
+import android.databinding.ObservableBoolean;
 import android.databinding.ObservableField;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import ch.epfl.sweng.favors.R;
 import ch.epfl.sweng.favors.authentication.Authentication;
+import ch.epfl.sweng.favors.authentication.AuthenticationProcess;
 import ch.epfl.sweng.favors.database.Database;
 import ch.epfl.sweng.favors.database.User;
 import ch.epfl.sweng.favors.databinding.FragmentProfileLayoutBinding;
+import ch.epfl.sweng.favors.utils.TextWatcherCustom;
 import ch.epfl.sweng.favors.utils.Utils;
+
+import static ch.epfl.sweng.favors.authentication.AuthenticationProcess.REQUIREMENTS_STRING;
 
 public class ProfileFragment extends Fragment {
 
@@ -28,10 +35,28 @@ public class ProfileFragment extends Fragment {
     public ObservableField<String> baseCity = User.getMain().getObservableObject(User.StringFields.city);
     public ObservableField<String> sex = User.getMain().getObservableObject(User.StringFields.sex);
     public ObservableField<String> email = User.getMain().getObservableObject(User.StringFields.email);
-    public ObservableField<String> profileName =  new ObservableField<>();// new ObservableField<>(user.getObservableObject(User.StringFields.firstName).get() + "'s Profile");
+    public ObservableField<String> profileName =  new ObservableField<>();
     public ObservableField<Long> tokens = User.getMain().getObservableObject(User.LongFields.tokens);
+    public ObservableField<String> requirementsText = new ObservableField<>();
+    public AuthenticationProcess.Action action;
 
+    /**
+     * sets the boolean to whether password is correct or not
+     */
+    public ObservableBoolean isPasswordCorrect = new ObservableBoolean(false){
+        @Override
+        public void set(boolean value) {
+            super.set(value);
+            requirementsText.set(action == AuthenticationProcess.Action.Register && this.get() ? "" : REQUIREMENTS_STRING);
+        }
+    };
 
+    private TextWatcher passwordTextWatcher = new TextWatcherCustom() {
+        @Override
+        public void afterTextChanged(Editable s) {
+            isPasswordCorrect.set(Utils.passwordFitsRequirements(binding.passwordEntry.getText().toString()));
+        }
+    };
     FragmentProfileLayoutBinding binding;
 
     @Nullable
@@ -40,7 +65,8 @@ public class ProfileFragment extends Fragment {
         Database.getInstance().updateFromDb(User.getMain());
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile_layout,container,false);
         binding.setElements(this);
-
+// Check if the password is correct each time a letter was added
+        binding.passwordEntry.addTextChangedListener(passwordTextWatcher);
         binding.editProfileButton.setOnClickListener((v)-> {
             Fragment fragment = null;
             fragment = new EditProfileFragment();
