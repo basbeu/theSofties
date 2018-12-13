d5fcbc98b6b012e263f2c0ee18cde46ee27abf23
Andrea Scalisi <andrea.scalisi@epfl.ch>
2018-12-13 11:29:21 +0100

    Add longer thread sleep in onRequestPermissionResultTest to make Travis more comfortable

--- app/src/androidTest/java/ch/epfl/sweng/favors/favors/FavorsCreateFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/favors/favors/FavorsCreateFragmentTest.java
@@ -223,7 +223,7 @@ public class FavorsCreateFragmentTest {
         }
         mFragmentTestRule.launchActivity(null);
         try {
-            Thread.sleep(500);
+            Thread.sleep(1000);
 
         }catch (Exception e){
 
