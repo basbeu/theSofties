377ee941e10f290098b6d6cb907005e29e4fdeac
Unknown <charline.montial@epfl.ch>
2018-12-16 14:51:21 +0100

    Remove commented code

--- app/src/androidTest/java/ch/epfl/sweng/favors/profile/ProfileFragmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/favors/profile/ProfileFragmentTest.java
@@ -37,19 +37,7 @@ public class ProfileFragmentTest {
         mFragmentTestRule.launchActivity(null);
         onView(ViewMatchers.withId(R.id.profileTitle)).check(matches(isDisplayed()));
     }
-
-
-//    @Test
-//    public void firstName_is_displayed(){
-//        mFragmentTestRule.launchActivity(null);
-//        onView(withId(R.id.profFirstName)).check(matches(withText(FakeAuthentication.FIRST_NAME)));
-//    }
-//
-//    @Test
-//    public void lastName_is_displayed(){
-//        mFragmentTestRule.launchActivity(null);
-//        onView(withId(R.id.profLastName)).check(matches(withText(FakeAuthentication.LAST_NAME)));
-//    }
+    
 
     @Test
     public void email_is_displayed(){
