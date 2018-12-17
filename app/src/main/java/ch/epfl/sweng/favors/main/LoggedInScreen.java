0dcc2a6f474f33ecdacd6a3a4caef363a11c1969
Unknown <charline.montial@epfl.ch>
2018-12-17 17:49:54 +0100

    Import toast class

--- app/src/main/java/ch/epfl/sweng/favors/main/LoggedInScreen.java
+++ app/src/main/java/ch/epfl/sweng/favors/main/LoggedInScreen.java
@@ -14,6 +14,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.Timestamp;
