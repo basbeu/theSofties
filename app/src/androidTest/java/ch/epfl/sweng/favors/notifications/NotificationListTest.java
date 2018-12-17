57aac893238b7e91012fe8fdc0afa46031c37141
basbeu <bastien.beuchat@epfl.ch>
2018-12-17 11:30:08 +0100

    Add a test for NotificationListAdapter

--- app/src/androidTest/java/ch/epfl/sweng/favors/notifications/NotificationListTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/favors/notifications/NotificationListTest.java
@@ -1,14 +1,23 @@
 package ch.epfl.sweng.favors.notifications;
 
+import android.support.test.espresso.matcher.ViewMatchers;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
+import ch.epfl.sweng.favors.R;
 import ch.epfl.sweng.favors.database.FakeDatabase;
 import ch.epfl.sweng.favors.notifications.NotificationsFragment;
 import ch.epfl.sweng.favors.utils.ExecutionMode;
 import ch.epfl.sweng.favors.utils.FragmentTestRule;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 
 public class NotificationListTest {
 
@@ -26,6 +35,20 @@ public class NotificationListTest {
     public void startFragment() throws InterruptedException {
         mFragmentTestRule.launchActivity(null);
         Thread.sleep(2000);
+    }
+
+    @Test
+    public void displayNotification() throws InterruptedException {
+        mFragmentTestRule.launchActivity(null);
+        Thread.sleep(2000);
+        mFragmentTestRule.getActivity().runOnUiThread(() -> {
+                    mFragmentTestRule.getFragment().notificationsList.add("Super Notification");
+                    mFragmentTestRule.getFragment().listAdapter.notifyDataSetChanged();
+                });
+
+        Thread.sleep(2000);
+
+        onView(ViewMatchers.withText("Super Notification")).check(matches(isDisplayed()));
     }
 
     @After
